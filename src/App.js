374588efe47158fde5da74405f1a693ef4113b14
dotlaksh <dotlaksh@outlook.com>
2024-10-26 22:36:54 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StockSymbols from './StockSymbols';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stock Charts</h1>
+      <StockSymbols />
+    </div>
+  );
+}
+
+export default App;
